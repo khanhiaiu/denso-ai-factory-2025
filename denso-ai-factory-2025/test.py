b178b85d0c14ef4bf5bcecdc7d4c8908c72d1f9c
Nguyễn Trọng Khánh <nguyenkhanhai2004@gmail.com>
2026-01-19 12:50:22 +0700

    Delete denso-ai-factory-2025 directory

--- denso-ai-factory-2025/test.py
+++
@@ -1,6 +0,0 @@
-# python merge_ckpt.py --params outputs/model/diff-params-ARGS=2/mat_truc/params-last.pt --seg outputs/model/diff-params-ARGS=2/mat_truc/seg-best.pt --output outputs/model/diff-params-ARGS=2/mat_truc/merged-params-best.pt
-import torch
-
-ckpt_path = "outputs/model/diff-params-ARGS=2/mat_truc/seg-best.pt"
-ckpt = torch.load(ckpt_path, map_location='cpu')
-print(ckpt['epoch'])
